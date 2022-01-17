3bcae4bdf9d5be69e41fc772b03d52698b9c7f2a
Dominik Slehofer <93545274+Dominique0301@users.noreply.github.com>
2022-01-17 14:15:50 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -2,20 +2,9 @@
 
 from os.path import basename, splitext
 import tkinter as tk
+import random
 
 # from tkinter import ttk
-
-
-class About(tk.Toplevel):
-    def __init__(self, parent):
-        super().__init__(parent, class_=parent.name)
-        self.config()
-
-        btn = tk.Button(self, text="Konec", command=self.close)
-        btn.pack()
-
-    def close(self):
-        self.destroy()
 
 
 class Application(tk.Tk):
@@ -26,16 +15,62 @@ class Application(tk.Tk):
         super().__init__(className=self.name)
         self.title(self.name)
         self.bind("<Escape>", self.quit)
-        self.lbl = tk.Label(self, text="Hello World")
+        self.lbl = tk.Label(self, text="Kalkulačka")
         self.lbl.pack()
-        self.btn = tk.Button(self, text="Quit", command=self.quit)
+        self.btn = tk.Button(self, text="Konec", command=self.quit)
         self.btn.pack()
         self.btn2 = tk.Button(self, text="About", command=self.about)
         self.btn2.pack()
+        self.btn3 = tk.Button(self, text="Kontrola", command=self.kontrola)
+
+
+    def plus(self):
+        self.cisloA = random.randint(1,99)
+        self.cisloB = random.randint(1,100-self.cisloA)#aby vysledek nebyl větší než 100
+        self.vysledek = self.cisloA + self.cisloB
+        self.lbl.config(text="+")
+
+
+    def minus(self):
+        self.cisloA = random.randint(1,99)
+        self.cisloB = random.randint(1,99)
+        if self.cisloB > self.cisloB:
+            self.cisloA, self.cisloB = self.cisloB, self.cisloA
+        self.vysledek == self.cisloA - self.cisloB
+        self.lbl.config(text="-")
+
+
+
+    def krat(self):
+        self.cisloA = random.randint(1,10)
+        self.cisloB = random.randint(1,10)
+        self.vysledek = self.cisloA * self.cisloB
+        self.lbl.config(text="*")
+
+
+    def deleno(self):
+        self.vysledek = random.randint(1,9)
+        self.cisloB = random.randint(1,9)
+        self.cisloA = self.vysledek * self.cisloB
+        self.lbl.config(text="/")
+
+    def generuj(self):
+        funkce = random.choice([self.plus,self.minus,self.krat,self.deleno])
+        funkce()
+
+
+
+    def kontrola(self):
+        
+
+
+
+
+
+
 
     def about(self):
-        window = About(self)
-        window.grab_set()
+        self.generuj()
 
     def quit(self, event=None):
         super().quit()
