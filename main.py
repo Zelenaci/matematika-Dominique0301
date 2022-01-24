9c9eb20881ab58661a9c7c204f8c180625e78eb4
Dominik Slehofer <93545274+Dominique0301@users.noreply.github.com>
2022-01-24 12:49:01 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,27 +1,32 @@
-#!/usr/bin/env python3
-
 from os.path import basename, splitext
 import tkinter as tk
 import random
+from tkinter.constants import END
+#from typing_extensions import IntVar
+from xml.dom.minidom import Entity
 
 # from tkinter import ttk
-
+#Dominik Šlehofer
 
 class Application(tk.Tk):
     name = basename(splitext(basename(__file__.capitalize()))[0])
-    name = "Foo"
+    name = "Matematická Hra"
+    #tk.geometry(150*200)
+
 
     def __init__(self):
         super().__init__(className=self.name)
         self.title(self.name)
         self.bind("<Escape>", self.quit)
-        self.lbl = tk.Label(self, text="Kalkulačka")
-        self.lbl.pack()
-        self.btn = tk.Button(self, text="Konec", command=self.quit)
-        self.btn.pack()
-        self.btn2 = tk.Button(self, text="About", command=self.about)
-        self.btn2.pack()
+        self.lbl = tk.Label(self, text="")
+        self.generuj()
+        self.vysledek_vstup = tk.Entry(self)
+        self.vysledek_vstup.grid(row=3, column=1)
+        self.lbl_hodnoceni = tk.Label(self, text="")
+        self.lbl_hodnoceni.grid(row=4,column=1)
         self.btn3 = tk.Button(self, text="Kontrola", command=self.kontrola)
+        self.btn3.grid(row=5, column=1)
+
 
 
     def plus(self):
@@ -29,15 +34,16 @@ class Application(tk.Tk):
         self.cisloB = random.randint(1,100-self.cisloA)#aby vysledek nebyl větší než 100
         self.vysledek = self.cisloA + self.cisloB
         self.lbl.config(text="+")
+        return str(self.cisloA)+"+"+str(self.cisloB)
 
 
     def minus(self):
         self.cisloA = random.randint(1,99)
-        self.cisloB = random.randint(1,99)
-        if self.cisloB > self.cisloB:
-            self.cisloA, self.cisloB = self.cisloB, self.cisloA
-        self.vysledek == self.cisloA - self.cisloB
-        self.lbl.config(text="-")
+        self.cisloB = random.randint(1,self.cisloA)
+        self.vysledek = self.cisloA - self.cisloB
+        return str(self.cisloA)+"-"+str(self.cisloB)
+
+
 
 
 
@@ -45,22 +51,35 @@ class Application(tk.Tk):
         self.cisloA = random.randint(1,10)
         self.cisloB = random.randint(1,10)
         self.vysledek = self.cisloA * self.cisloB
-        self.lbl.config(text="*")
+        return str(self.cisloA)+"*"+str(self.cisloB)
+
+        
 
 
     def deleno(self):
-        self.vysledek = random.randint(1,9)
-        self.cisloB = random.randint(1,9)
+        self.vysledek = random.randint(1,10)
+        self.cisloB = random.randint(1,10)
         self.cisloA = self.vysledek * self.cisloB
-        self.lbl.config(text="/")
+        return str(self.cisloA)+ "/" +str(self.cisloB)
+
+
 
     def generuj(self):
-        funkce = random.choice([self.plus,self.minus,self.krat,self.deleno])
-        funkce()
+        self.funkce = random.choice([self.plus,self.minus,self.krat,self.deleno])
+        self.priklad = self.funkce()
+        self.lbl.config(text= self.priklad)
+        self.lbl.grid(row=1,column=1)
+        
 
 
 
     def kontrola(self):
+        if int(self.vysledek_vstup.get()) == int(self.vysledek):
+            self.lbl_hodnoceni.config(text="SPRÁVNĚ")
+        else:
+            self.lbl_hodnoceni.config(text="ŠPATNĚ")
+        self.generuj()
+        self.vysledek_vstup.delete(0,END)
         
 
 
@@ -68,12 +87,6 @@ class Application(tk.Tk):
 
 
 
-
-    def about(self):
-        self.generuj()
-
-    def quit(self, event=None):
-        super().quit()
 
 
 app = Application()
